2d24457492b77800d5e6977e5e145822ffd856fd
RaghadAz <raghads200@gmail.com>
2022-12-22 18:20:50 +0300

    api

---
+++ lib/app/constants.dart
@@ -0,0 +1,3 @@
+class Constants{
+  static const String baseUrl ="http://r123.mocklab.io/";
+}
---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,18 @@
+
+import 'package:dio/dio.dart';
+import 'package:flutter_application_1/app/constants.dart';
+import 'package:flutter_application_1/data/response/responses.dart';
+import 'package:retrofit/http.dart';
+
+part 'app_api.g.dart';
+
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceClient{
+factory AppServiceClient(Dio dio,{String baseUrl})=_AppServiceClient;
+@POST("/customers/login")
+Future<AuthenticationResponse> login(
+    @Field("email") String email,
+    @Field("password") String password
+    );
+}
